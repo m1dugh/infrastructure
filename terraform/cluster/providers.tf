b8eb774bd96121cd3f4d6c5fd08e189167f00fea
m1dugh <romain.le-miere@epita.fr>
2024-10-12 18:20:25 +0200

    feat(cluster): init

---
+++ terraform/cluster/providers.tf
@@ -0,0 +1,16 @@
+terraform {
+    required_providers {
+        kubernetes = {
+            source = "hashicorp/kubernetes"
+                version = "2.33.0"
+        }
+        helm = {
+            source = "hashicorp/helm"
+            version = "2.16.0"
+        }
+    }
+}
+
+provider "kubernetes" { }
+
+provider "helm" { }
