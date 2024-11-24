5c160e00b880bb12ef0881dd803953ba2489fd71
m1dugh <romain.le-miere@epita.fr>
2024-11-24 22:17:30 +0100

    feat(minio): deploys minio operator

--- terraform/cluster/modules/minio/main.tf
+++ terraform/cluster/modules/minio/main.tf
@@ -4,3 +4,12 @@ resource "kubernetes_namespace" "namespace" {
     annotations = var.default_annotations
   }
 }
+
+resource "helm_release" "operator" {
+    name = "operator"
+    chart = "operator"
+    namespace = kubernetes_namespace.namespace.metadata[0].name
+    repository = "https://operator.min.io"
+
+    version = "6.0.1"
+}
